15893bfb9134cddddc3ac5f931920be24800e1ce
thereisnowinter <505975669@qq.com>
2018-06-03 14:42:46 +0800

    finish calendar

---
+++ src/app/components/CalendarWeek.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="calendar-week" class="container">
+    <div class="columns is-mobile">
+      <calendar-day v-for="day in sharedState.seedData" :key="day.id" :day="day"></calendar-day>
+    </div>
+  </div>
+</template>
+
+<script>
+import {store} from '../store.js'
+import CalendarDay from './CalendarDay.vue'
+export default {
+  name: 'CalendarWeek',
+  data() {
+    return {
+      sharedState: store.state
+    }
+  },
+  components: {
+    CalendarDay
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#calendar-week {
+  margin-bottom: 50px;
+  .column {
+    padding: 0 0 0 0;
+  }
+}
+</style>
